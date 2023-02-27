6bbad8e59baa52243305e3562f42714dcbf5fa49
original.flipster <flipster.kehrer@gmail.com>
2023-02-27 22:08:10 +0100

    cloud lock inital

---
+++ src/main/java/CloudLock.java
@@ -0,0 +1,19 @@
+public class CloudLock {
+
+    public static void main(String[] args) {
+
+        //bucket name
+
+        //file
+
+        //heartbeat
+
+        //backoff time
+
+        //acquire lock
+    }
+
+    private void acquireLock() {
+
+    }
+}
